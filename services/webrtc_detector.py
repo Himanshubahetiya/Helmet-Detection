daa9ce1d98ed7043157b1061f92985291fa46d4b
Himanshu Bahetiya <himanshubahetiya@gmail.com>
2026-06-04 16:51:51 +0530

    add the live sesseion and ui

--- services/webrtc_detector.py
+++ services/webrtc_detector.py
@@ -1,26 +1,59 @@
 from ultralytics import YOLO
 from streamlit_webrtc import VideoProcessorBase
 import av
+import cv2
+import os
 
-# Load model only once
 model = YOLO("model/best.pt")
+
+os.makedirs("outputs/videos", exist_ok=True)
+
+LIVE_CACHE_FILE = "outputs/videos/live_helmet_detection_log.mp4"
 
 
 class HelmetVideoProcessor(VideoProcessorBase):
 
+    def __init__(self):
+        self.out = None
+
+        try:
+            if os.path.exists(LIVE_CACHE_FILE):
+                os.remove(LIVE_CACHE_FILE)
+        except:
+            pass
+
     def recv(self, frame):
 
-        # Convert frame to numpy array
         img = frame.to_ndarray(format="bgr24")
 
-        # YOLO prediction
         results = model(img, verbose=False)
 
-        # Draw bounding boxes
         annotated_frame = results[0].plot()
 
-        # Return processed frame
+        if self.out is None:
+
+            h, w, _ = annotated_frame.shape
+
+            fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+            self.out = cv2.VideoWriter(
+                LIVE_CACHE_FILE,
+                fourcc,
+                20.0,
+                (w, h)
+            )
+
+        self.out.write(annotated_frame)
+
         return av.VideoFrame.from_ndarray(
             annotated_frame,
             format="bgr24"
         )
+
+    def __del__(self):
+
+        try:
+            if self.out is not None:
+                self.out.release()
+        except:
+            pass
